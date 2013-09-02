c351108a2fa3728b498f69c6d402bec295d12713
buckmarble <forrestmarble@gmail.com>
2013-09-02 13:42:11 -0400

    Revert "build fixes"
    This reverts commit e7e7ff24d5b4c5d4010e51274390f22c83e0f65c.

--- src/com/android/settings/elite/StatusBar.java
+++ src/com/android/settings/elite/StatusBar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Android
+ * Copyright (C) 2013 DEMENTED Droid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.elite;
+package com.android.settings.demented;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import android.os.Bundle;
