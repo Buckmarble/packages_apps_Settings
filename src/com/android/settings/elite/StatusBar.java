e7e7ff24d5b4c5d4010e51274390f22c83e0f65c
buckmarble <forrestmarble@gmail.com>
2013-09-02 13:35:28 -0400

    build fixes

--- src/com/android/settings/elite/StatusBar.java
+++ src/com/android/settings/elite/StatusBar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 DEMENTED Droid
+ * Copyright (C) 2013 Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.demented;
+package com.android.settings.elite;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import android.os.Bundle;
